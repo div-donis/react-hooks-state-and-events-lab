2c61ef9bfc52ba6fb7a91dcb68d04c6e21a3f916
Donovan <divdonis@gmail.com>
2021-07-03 20:35:51 -0700

    Partially Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,17 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [isActive, setActive] = useState("false")
+  const ToggleClass = () =>  {
+    setActive(!isActive);
+  }
+  const appClass = isActive ? "App light" : "App dark" 
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={ToggleClass}>Dark Mode</button>
       </header>
       <ShoppingList items={items} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Item({ name, category }) {
+  const [isActive, setActive] = useState("false")
+  const CartStatus = () =>  {
+    setActive(!isActive);
+  }
+ const cartStatus = isActive ?  "" : "in-cart"
   return (
-    <li className="">
+    <li className={cartStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={CartStatus}>Add to Cart</button>
     </li>
   );
 }
